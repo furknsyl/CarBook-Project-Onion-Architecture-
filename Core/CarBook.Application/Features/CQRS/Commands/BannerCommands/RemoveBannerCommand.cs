074172a1c2701ad5a5e7558f09522df5322166a7
furknsyl <fsoylu500@gmail.com>
2025-03-01 13:52:50 +0300

    Add project files.

---
+++ Core/CarBook.Application/Features/CQRS/Commands/BannerCommands/RemoveBannerCommand.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarBook.Application.Features.CQRS.Commands.BannerCommands
+{
+    public class RemoveBannerCommand
+    {
+        public RemoveBannerCommand(int ıd)
+        {
+            Id = ıd;
+        }
+
+        public int Id { get; set; }
+
+    }
+}
